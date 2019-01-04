60674f849061179edd3946708f6d6e4a45284513
Cezary Prokopowicz <prokopowicz.cezary@gmail.com>
2019-01-04 01:17:49 +0100

    Clean up and add component and bind events

--- src/App.js
+++ src/App.js
@@ -1,27 +1,49 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Person from './Person/Person'
 
 class App extends Component {
+  state = {
+    persons: [
+      { name:'Max', age: 28 },
+      { name:'Manu', age: 29 },
+      { name:'Stephanie', age: 26 }
+    ],
+    otherState: 'Some other state'
+  }
+
+  switchNameHandler = (newName) => {
+    // console.log('was clicked');
+    // DON'T DO THIS this.state.persons[0].name = 'Cezary'
+    this.setState({
+      persons: [
+        { name: newName, age: 28 },
+        { name:'Manu', age: 29 },
+        { name:'Stephanie', age: 27 }
+      ]
+    })
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <h1>Hi, I'm a React App</h1>
+        <p>This is realy wroking</p>
+        <button onClick={() => this.switchNameHandler('Maxymilian!!')}>Switch Name</button>
+        <Person 
+          name={this.state.persons[0].name}
+          age={this.state.persons[0].age} />
+        <Person
+          name={this.state.persons[1].name}
+          age={this.state.persons[1].age} 
+          click={this.switchNameHandler.bind(this, 'Maxio!')} //Recomended
+          >My Hobbies: Racing</Person>
+        <Person
+          name={this.state.persons[2].name}
+          age={this.state.persons[2].age} />
       </div>
     );
+    // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Work now'))
   }
 }
 
