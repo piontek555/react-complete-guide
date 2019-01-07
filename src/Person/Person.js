f0df92d2ecd705d50648bbf262a390cbe66ef36e
CezaryProkopowicz <cezary.prokopowicz@intive.com>
2019-01-07 20:29:23 +0100

    Remove last part od Radium styles and eject project

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -3,11 +3,6 @@ import React from 'react';
 import './Person.css';
 
 const person = ( props ) => {
-    const styles = {
-        '@media (min-width: 500px)': {
-            width: '450px'
-        }
-    }
     return (
         <div className="Person">
             <p onClick={props.click}>I'm a {props.name} and I am {props.age} years old</p>
